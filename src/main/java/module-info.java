7c4dbb99386f2f3b6dc1fb9b409770a5ccb4d4d6
ezrankayamba <ezrankayamba@gmail.com>
2018-04-14 18:24:47 +0300

    Made the project as module (Java9) and removed test classes due dependency issues

---
+++ src/main/java/module-info.java
@@ -0,0 +1,24 @@
+
+module tz.co.nezatech.senter {
+	requires java.sql;
+	requires java.se;
+	requires spring.beans;
+	requires spring.core;
+	requires spring.context;
+	requires spring.tx;
+	requires spring.web;
+	requires spring.webmvc;
+	requires spring.boot;
+	requires spring.jdbc;
+	requires spring.boot.autoconfigure;
+	requires javax.servlet.api;
+	requires tz.co.nezatech.nezadb;
+	requires spring.security.core;
+	requires spring.security.web;
+	requires spring.security.config;
+	requires org.thymeleaf;
+	requires javax.mail.api;
+	requires slf4j.api;
+	requires spring.context.support;
+	requires reflections;	
+}
